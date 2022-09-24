f146b83e139dd485ee06792981552820de46e986
Agustin <aguchobertero@gmail.com>
2022-09-24 09:21:35 -0300

    Initialize the machine and user sequence. And I create the Start Game button.

---
+++ index.js
@@ -0,0 +1,6 @@
+let secuenciaMaquina = []; //Para guardar la secuencia de la maquina
+let secuenciaUsuario = []; //Para guardar la secuencia del usuario
+let ronda = 0; //Empezamos en la ronda 0
+
+document.querySelector('button[type=button]').onclick = comenzarJuego; //Selecciono un boton que tenga un type = button cuando le hago click ejecutra la funcion ComenzarJuego
+
